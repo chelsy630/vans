2d3128962ece50902ee7149181a2caf8d3901965
chelsy630 <chlesy860630@gmail.com>
2020-03-26 19:01:33 +0800

    新增販賣機

---
+++ data.py
@@ -0,0 +1,12 @@
+class Drink:
+    def __init__(self,new_name,new_price):
+        self.name = new_name
+        self.price =new_price
+    def get_name(self):
+        return self.__name
+class Student:
+            def __init__(self, new_gender, new_major, new_id):
+                self.gender = new_gender
+                self.__major = new_major
+                self.id = new_id
+
